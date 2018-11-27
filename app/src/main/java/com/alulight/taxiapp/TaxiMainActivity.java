245f37341cf2c508fb661ecc58255a97d244c21f
jain221 <sgodha111@gmail.com>
2018-11-27 10:19:46 +0000

    StartUp

---
+++ app/src/main/java/com/alulight/taxiapp/TaxiMainActivity.java
@@ -0,0 +1,13 @@
+package com.alulight.taxiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaxiMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taxi_main);
+    }
+}
